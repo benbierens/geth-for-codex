53081b2459b5f7464fd118335fe5ca386e49d5f5
benbierens <thatbenbierens@gmail.com>
2023-04-17 14:34:59 +0200

    enables mining on bootstrap node

--- run.sh
+++ run.sh
@@ -11,16 +11,18 @@ base64 -d genesis.b64 > genesis.json
 geth account new --password passwordfile
 
 ACCOUNTSTR=$(geth account list)
-ACCOUNTSTR="0x"$(echo $ACCOUNTSTR | cut -d '{' -f 2 | cut -d '}' -f 1)
+ACCOUNTSTR=$(echo $ACCOUNTSTR | cut -d '{' -f 2 | cut -d '}' -f 1)
+echo "0x"$ACCOUNTSTR > account_string.txt
 
+BOOTSTRAP_ARGS=""
 if [ -n "$IS_BOOTSTRAP" ]; then
     # update genesis.json
     sed -i "s/ACCOUNT_HERE/$ACCOUNTSTR/g" genesis.json
+    BOOTSTRAP_ARGS="--mine --miner.etherbase 0x$ACCOUNTSTR"
 fi
 
-# echo "Intializing geth..."
-# geth init --datadir data genesis.json
+echo "Intializing geth..."
+geth init genesis.json
 
-# echo "Starting geth..."
-# geth $GETH_ARGS
-# --networkid=???
+echo "Starting geth..."
+geth --networkid 789988 --http --http.addr 0.0.0.0 --allow-insecure-unlock --http.vhosts '*' --unlock "0x$ACCOUNTSTR" $BOOTSTRAP_ARGS $GETH_ARGS < passwordfile
