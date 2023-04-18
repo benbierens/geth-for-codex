0f6fb0a7e60be55a07f5c3e4a1d15b746d1b4a9d
benbierens <thatbenbierens@gmail.com>
2023-04-18 13:09:47 +0200

    Adds bootstrap account private key.

--- content/run.sh
+++ content/run.sh
@@ -1,6 +1,7 @@
 if [ -n "$IS_BOOTSTRAP" ]; then
     echo "Starting bootstrap geth..."
     ACCOUNTSTR="68554188b8dcb8fb51fe35301cb0dc985e8ae8d6"
+    echo "0x"$ACCOUNTSTR > account_string.txt
     BOOTSTRAP_ARGS="--mine --miner.etherbase 0x$ACCOUNTSTR"
     geth --networkid 789988 --http --http.addr 0.0.0.0 --allow-insecure-unlock --http.vhosts '*' --unlock "0x$ACCOUNTSTR" $BOOTSTRAP_ARGS $GETH_ARGS < passwordfile
     exit 0
