4ff22332a7cf2a2c69981721f9d469920d101434
benbierens <thatbenbierens@gmail.com>
2023-05-03 09:28:12 +0200

    Fixes sync issue

--- content/run.sh
+++ content/run.sh
@@ -12,16 +12,21 @@ if [ -n "$UNLOCK_START_INDEX" ]; then
         INDEX=$(($INDEX + 1))
     done <accounts.csv
 
-    UNLOCK_ARGS="--allow-insecure-unlock --unlock "$UNLOCK_ACCOUNTS" --password passwordfile"
+    UNLOCK_ARGS="--unlock "$UNLOCK_ACCOUNTS" --password passwordfile"
 fi
 
 echo "Starting geth..."
 
-# geth init genesis.json
-
 if [ -n "$ENABLE_MINER" ]; then
     MINER_ARGS="--mine --miner.etherbase 0x10420A3dE36231E12eb601F45b4004311372dcEa"
+else
+    rm -Rf /root/.ethereum/geth
 fi
 
-geth --networkid 789988 --http --http.addr 0.0.0.0 --http.vhosts '*' $UNLOCK_ARGS $MINER_ARGS $GETH_ARGS
+echo "UNLOCK_ARGS: $UNLOCK_ARGS"
+echo "MINER_ARGS: $MINER_ARGS"
+echo "GETH_ARGS: $GETH_ARGS"
+
+geth init genesis.json
+geth --networkid 789988 --http --http.addr 0.0.0.0 --allow-insecure-unlock --http.vhosts '*' $UNLOCK_ARGS $MINER_ARGS $GETH_ARGS
 exit 0
